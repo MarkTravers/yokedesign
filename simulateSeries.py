4527e0e3a8dea9ef8a0e1214ac4543472c69b1db
Mark Travers <marktheshark823@gmail.com>
2021-05-20 18:09:36 -0600

    refactoring parameter sweep

--- simulateSeries.py
+++ simulateSeries.py
@@ -1,14 +1,22 @@
 #!python3
 
-from magLabUtilities.parameterspaceutilities.parameterspace import Parameter
-from magLabUtilities.magstromutilities.testcaseutilities import MagstromTestGroup
+from magLabUtilities.parametrictestutilities.parameterspace import Parameter
+from magLabUtilities.parametrictestutilities.testmanager import TestGroup
 
 if __name__ == '__main__':
-    # testGroup = MagstromTestGroup('', 'D:/BitBucket/yokedesign/testRuns/041621/')
-    testGroup = MagstromTestGroup('', 'C:/Users/travemar/Bitbucket/yokedesign/testRuns/042721/')
+    # Initialize test group
+    testGroup = TestGroup()
+
+    # Add parameters to be varied
     testGroup.addParameter(Parameter('thickness', [1.0, 1.5]))
     testGroup.addParameter(Parameter('zThickness', [1.5, 2.0]))
     testGroup.addParameter(Parameter('cornerRadius', [6.0, 7.0, 8.0]))
+    testGroup.addParameter(Parameter('yokeMur', [200.0, 400.0]))
+
+    # Generate test cases for parameter space and create directories for each test case
     testGroup.generateTestCases()
-    testGroup.createTestCaseDirectories()
+    testGroup.createTestCaseDirectories('./testRuns/052021/')
+
+    # Supply functions to be evaluated for each test case (each function is evaluated in the order they are supplied here)
+    testGroup.addProcessFunction()
     
